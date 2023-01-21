d56e8bd9101b8b71153f37ea4964e1af1acac1aa
Markuss Abolins <markuss.abolins@protonmail.com>
2023-01-22 00:01:47 +0200

    Update landingPage.js

--- landingPage.js
+++ landingPage.js
@@ -5,5 +5,5 @@ $().ready(function(){
     /*code here*/
     console.log("On ready")
     alert("test2");
-    $("#headerContainer").load("header.html");
+    $("#headerContainer").html("header.html");
 })
