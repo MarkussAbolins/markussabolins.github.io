19852fa59bec2468330e7d910d55a26aa14a2799
Markuss Abolins <markuss.abolins@protonmail.com>
2024-12-19 13:21:06 +0200

    Better error handling

--- landingPage.js
+++ landingPage.js
@@ -13,21 +13,23 @@ function handleLogin() {
     if (input === "test") {
       // Redirect to testLetter.html
       window.location.href = "testLetter.html";
+      return;
     }
     if (input === "AL91"){
-
+        return;
     }
     if (input === "96MP"){
-
+        return;
     }
     if (input === "CHSZ"){
-
+        return;
     }
     if (input === "24HE"){
-
+        return;
     }
     else {
       // Show an error or handle invalid input
       alert("Incorrect code. Please try again.");
+      return;
     }
   }
