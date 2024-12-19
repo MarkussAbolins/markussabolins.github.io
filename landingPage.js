f8979afc48146f613d29f6b7775bfdd8c34ecea5
Markuss Abolins <markuss.abolins@protonmail.com>
2024-12-19 13:06:55 +0200

    Basic redirect setup

--- landingPage.js
+++ landingPage.js
@@ -4,3 +4,30 @@ $().ready(function(){
     //console.log("On ready")
     //$("#headerContainer").load("header.html");
 })
+
+function handleLogin() {
+    // Get the value from the input field
+    input = document.getElementById("loginInput").value;
+    console.log(input);
+    // Check if the input equals "test"
+    if (input === "test") {
+      // Redirect to testLetter.html
+      window.location.href = "testLetter.html";
+    }
+    if (input === "AL91"){
+
+    }
+    if (input === "96MP"){
+
+    }
+    if (input === "CHSZ"){
+
+    }
+    if (input === "24HE"){
+
+    }
+    else {
+      // Show an error or handle invalid input
+      alert("Incorrect code. Please try again.");
+    }
+  }
